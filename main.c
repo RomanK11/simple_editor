ec8c623286d4e8eefe5e0ba618093e865201ab8e
viscount <111529097+RomanK11@users.noreply.github.com>
2026-09-19 17:54:54 +0300

    Implement file loading and saving

--- main.c
+++ main.c
@@ -33,6 +33,29 @@ int main(int argc, char const *argv[])
         perror("fopen");
         return 1;
     }
+
+    while (fgets(temp, MAX_LEN, f) != NULL)
+    {
+        temp[strcspn(temp, "\n")] = '\0';
+
+        for (size_t i = 0; i < strlen(temp); i++)
+        {
+            printf("%c", temp[i]);
+        }
+        printf("\n");
+        
+        
+        //add_line
+        if (add_line(&buffer, &line_count, temp) != 0)
+        {
+            fprintf(stderr, "Failed to add line\n");
+            free_buffer(buffer, line_count);
+            fclose(f);
+            return 1;
+        }
+    }
+    fclose(f);
+    f = NULL;
     
     while (1)
     {
@@ -48,23 +71,30 @@ int main(int argc, char const *argv[])
         int res = add_line(&buffer, &line_count, temp);
         if (res == 1)
         {
+            free_buffer(buffer, line_count);
             return 1;
-        }
-        
+        }   
         
         if (strcmp(buffer[line_count - 1], ":save") == 0)
         {
+            FILE *save_file = fopen(argv[1], "w");
+
+            if (save_file == NULL)
+            {
+                free_buffer(buffer, line_count);
+                perror("fopen");
+                return 1;
+            }
+
             for (size_t i = 0; i < line_count - 1; i++)
             {
-                fputs(buffer[i], f);
-                fputc('\n', f);
+                fputs(buffer[i], save_file);
+                fputc('\n', save_file);
             }
-            free_buffer(buffer, line_count);
-            buffer = NULL;
-            line_count = 0;
+
+            fclose(save_file);
 
             printf("saved\n");
-
             continue;
         }
         if (strcmp(buffer[line_count - 1], ":quit") == 0)
@@ -74,7 +104,6 @@ int main(int argc, char const *argv[])
         }
     }
     
-    fclose(f);
     return 0;
 }
 
